c43e09104dc88450ee985b05b14c428f8446a687
antraxalan <aruquipa.alan@gmail.com>
2015-09-09 19:27:10 -0400

    'error_reader'

--- js/app_txt_reader.js
+++ js/app_txt_reader.js
@@ -1,13 +1,13 @@
 document.addEventListener("deviceready", init, false);
 function init() {
-
+	// alert('incio');
 	console.log(cordova.file.applicationDirectory);	
 	window.resolveLocalFileSystemURL(cordova.file.applicationDirectory, function(f) {
 		console.dir(f);
 	}, fail);
 
 	//This alias is a read-only pointer to the app itself
-	window.resolveLocalFileSystemURL(cordova.file.applicationDirectory + document.getElementById("dir_txt").value, gotFile, fail);
+	window.resolveLocalFileSystemURL(cordova.file.applicationDirectory + "index.html", gotFile, fail);
 
 	/* Yes, this works too for our specific example...
 	$.get("index.html", function(res) {
@@ -18,12 +18,13 @@ function init() {
 }
 
 function fail(e) {
+	alert('error'+e);
 	console.log("FileSystem Error");
 	console.dir(e);
 }
 
 function gotFile(fileEntry) {
-
+	alert('got');
 	fileEntry.file(function(file) {
 		var reader = new FileReader();
 
