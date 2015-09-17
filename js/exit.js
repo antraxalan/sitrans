611606886b2836fa7f4305f38846b31c5b866a9c
antraxalan <aruquipa.alan@gmail.com>
2015-09-17 12:23:08 -0400

    'exit_js'

---
+++ js/exit.js
@@ -0,0 +1,35 @@
+document.addEventListener("deviceready", deviceisready, false);
+
+function deviceisready(){
+    alert("Device Ready is called");
+    document.addEventListener("backbutton", function(e){
+        if ( $('.ui-page-active').attr('id') == 'mainpage') {
+            //window.location = "#exitDialog";
+            exitAppPopup();
+        }else{
+            history.back();
+        };
+    });
+};
+
+function exitAppPopup() {
+    navigator.notification.confirm(
+        "Do you really want to close this app?", 
+        function(buttonIndex){
+            ConfirmExit(buttonIndex);
+        }, 
+        "Confirmation", 
+        "Yes,No"
+    ); 
+    alert("Outside Notification"); 
+    //return false;
+};
+
+function ConfirmExit(stat){
+    alert("Inside ConfirmExit");
+    if(stat == "1"){
+        navigator.app.exitApp();
+    }else{
+        return;
+    };
+};
